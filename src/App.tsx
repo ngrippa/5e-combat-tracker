afffb775629f3fe3984e1e26add8bccf11d570b8
Nemo Grippa <nemo.grippa@gmail.com>
2024-01-07 11:34:39 +0100

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-const theme = createTheme({palette: {mode: 'dark'}});
+const theme = createTheme({ palette: { mode: "dark" } });
 export const App = () => {
-  return <ThemeProvider theme={theme}>
-<CssBaseline />
-    <main>This app is using the dark mode</main>
-  </ThemeProvider>
-}
-
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>This app is using the dark mode</main>
+    </ThemeProvider>
+  );
+};
